aae99e4a70b8fc62b25691e9c56c8d9f4e1020a3
Jan Kiene <jankiene@onlinehome.de>
2017-06-05 13:01:21 +0200

    First implementation of D&S for point sources
    Not tested yet!!

--- delay_and_sum/__init__.py
+++ delay_and_sum/__init__.py
@@ -1,7 +1,8 @@
-from .delay_and_sum import DelayAndSumPlane
+from .delay_and_sum import DelayAndSumPlane, DelayAndSumPointSources
 from .signal_processing import SignalProcessor
 from._helper import TestsignalGenerator
 
 __all__ = [DelayAndSumPlane,
+           DelayAndSumPointSources,
            SignalProcessor,
            TestsignalGenerator]
