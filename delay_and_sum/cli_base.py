5db3f5deee99c7d0f4ece418045556017cc330ff
Jan Kiene <jankiene@onlinehome.de>
2017-06-17 15:19:32 +0200

    Base class for command line handling

---
+++ delay_and_sum/cli_base.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import soundfile as sf
+import numpy as np
+
+class CliHandler:
+    """
+    Static namespace class for cli handling
+    """
+
+    def __init__(self):
+        raise NotImplementedError("Subclass this!!")
+
+
+    def plot_results(self, max_ang, rms_list):
+        """
+        Plot the directional function of a microphone array
+        """
+        plt.figure()
+        xmin = -max_ang
+        xmax = max_ang + 1
+        max_val = np.amax(rms_list)
+        src_angle = np.argmax(rms_list) - max_ang
+        ang_range = range(xmin, xmax, 1)
+        plt.plot(ang_range, rms_list)
+        plt.grid()
+        plt.xlabel(r"$\alpha / °$")
+        plt.ylabel(r"$R / dB$")
+        plt.xlim(xmin, xmax)
+        plt.title("Source found at: {}°".format(src_angle))
+        plt.vlines(src_angle, 0, max_val, 'r', '--')
+        plt.show()
+
+
+    def read_signals_from_wav(self, filename):
+        return sf.read(filename)
+
+
+    def main():
+        raise NotImplementedError("This needs to be overwritten in subclass")
